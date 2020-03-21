958f94521282c7d2abd916327271a015b04d6d51
papatrix <58409502+papatrix@users.noreply.github.com>
2020-03-21 14:10:55 -0400

    Add remaining search functionality (#19)

--- graphing.py
+++ graphing.py
@@ -5,6 +5,6 @@ import plotly.express as px
 def document_topics_pie(topics):
     df = pd.Series(topics, name="score").to_frame()
     df = df[df.score > 0.02]
-    fig = px.pie(df, values="score", names=df.index, title="Document Topics")
+    fig = px.pie(df, values="score", names=df.index)
     fig.update_layout(showlegend=False)
     return fig
