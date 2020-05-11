dd1b418557b30102c8c2701bc7da7f552f15a3ec
Pat Steeves <patricksteeves02@gmail.com>
2020-05-11 13:51:49 -0400

    display progress bar when results are loading

---
+++ webapp/src/Progress.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
+function Progress(props) {
+    return <CircularProgress className="progress-bar" />
+}
+
+export default Progress;
